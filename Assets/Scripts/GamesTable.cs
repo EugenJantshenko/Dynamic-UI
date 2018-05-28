22d70793600353a730c4735d677cdac1e55fb8a5
Eugen Jantshenko <kryoengineer@gmail.com>
2018-05-28 22:30:07 +0300

    Working Assembly 28.05.2018

---
+++ Assets/Scripts/GamesTable.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class GamesTable : MonoBehaviour
+{
+    [XmlRoot("Game")]
+    public class Game
+    {
+        [XmlAttribute]
+        private string gameName;
+        private int standatrGameRooms;
+        private int goldGameRooms;
+        private int currentGoldCoints;
+        private int standartGameGoldCosts;
+        private int goldGameGoldCosts;
+        private string[] standartRoomsNames;
+        private string[] goldRoomsNames;
+        private int[] standartRoomPlayers;
+        private int[] goldRoomPlayers;
+        private int maxRoomPlayers;
+        public string GameName { get { return gameName; } set {gameName=value; } }
+        public int StandatrGameRooms { get { return standatrGameRooms; } set { standatrGameRooms = value; } }
+        public int GoldGameRooms { get { return goldGameRooms; } set { goldGameRooms = value; } }
+        public int CurrentGoldCoints { get { return currentGoldCoints; } set { currentGoldCoints = value; } }
+        public int StandartGameGoldCosts { get { return standartGameGoldCosts; } set { standartGameGoldCosts = value; } }
+        public int GoldGameGoldCosts { get { return goldGameGoldCosts; } set { goldGameGoldCosts = value; } }
+        public string[] StandartRoomsNames { get { return standartRoomsNames; } set { standartRoomsNames = value; } }
+        public string[] GoldRoomsNames { get { return goldRoomsNames; } set { goldRoomsNames = value; } }
+        public int[] StandartRoomPlayers { get { return standartRoomPlayers; } set { standartRoomPlayers = value; } }
+        public int[] GoldRoomPlayers { get { return goldRoomPlayers; } set { goldRoomPlayers = value; } }
+        public int MaxRoomPlayers { get { return maxRoomPlayers; } set { maxRoomPlayers = value; } }
+
+        public Game()
+        { }
+        public Game(string name,int rooms,int goldrooms,int coints)
+        {
+            gameName = name;
+            standatrGameRooms = rooms;
+            goldGameRooms = goldrooms;
+            currentGoldCoints = coints;
+            standartGameGoldCosts = 300;
+            goldGameGoldCosts = 500;
+            standartRoomsNames= new string[standatrGameRooms];
+            goldRoomsNames = new string[goldGameRooms];
+            standartRoomPlayers = new int[standatrGameRooms];
+            goldRoomPlayers = new int[goldGameRooms];
+            for (int i = 0; i <= standatrGameRooms-1; i++)
+            {
+                standartRoomsNames[i] = "Standart Server" +" "+ (i + 1);
+            }
+            for (int i = 0; i <= goldGameRooms- 1; i++)
+            {
+                goldRoomsNames[i] = "Gold Server" + " " + (i + 1);
+            }
+            for (int i = 0; i <= standatrGameRooms - 1; i++)
+            {
+                standartRoomPlayers[i] = UnityEngine.Random.Range(0, 100);
+            }
+            for (int i = 0; i <= goldGameRooms - 1; i++)
+            {
+                goldRoomPlayers[i] = UnityEngine.Random.Range(0, 100);
+            }
+            maxRoomPlayers = 100;
+        }
+    }
+	void Start ()
+    {
+        Game starcraft = new Game("StarCraft", 2, 2, 400);
+        Game warcraft = new Game("WarCraft", 2, 3, 250);
+        Game tanki = new Game("Tanki", 3, 2, 180);
+        Game alphaprotokol= new Game("AlphaProtocol", 3, 4, 520);
+        Game finansist = new Game("Finansist", 4, 2, 499);
+        Game reunion = new Game("Reunion", 3, 3, 120);
+        List <Game> Games = new List<Game>() {starcraft,warcraft,tanki,alphaprotokol,finansist,reunion };
+        Serializer(Games);
+    }
+    readonly XmlSerializer serializer = new XmlSerializer(typeof(Game));
+    private void Serializer(List<Game> names)
+    {
+        foreach (var item in names)
+        {
+            FileStream stream = new FileStream(item.GameName+".xml", FileMode.Create, FileAccess.Write, FileShare.Read);
+            serializer.Serialize(stream, item);
+            Debug.Log("Create file -"+ item.GameName+".xml");
+            stream.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/RoomListSelector.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RoomListSelector : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private GameObject standartGamePanelPrafab;
+    [SerializeField] private GameObject goldGamePanelPrafab;
+    [SerializeField] private GameObject joinRoomPanelPrefab;
+    Dropdown gameSelector;
+    string currentChosenGame;
+    List<string> gameNames = null;
+    GamesTable.Game activeGame = new GamesTable.Game();
+    readonly XmlSerializer serializer = new XmlSerializer(typeof(GamesTable.Game));
+    public GameObject canvas;
+    GameObject gamePanell;
+    List<GameObject> PanellList=new List<GameObject>();
+    #endregion
+
+    void Start ()
+    {
+        CreateGameList();
+	}
+
+    private void CreateGameList()
+    {
+        gameSelector = canvas.GetComponentInChildren<Dropdown>();
+        gameSelector.ClearOptions();
+        //Debug.Log(gameSelector);
+        gameNames = new List<string>() {"StarCraft", "WarCraft", "Tanki", "AlphaProtocol","Finansist","Reunion" };
+        gameNames.Sort();
+        gameNames.Insert(0, "Chose Game!");
+        gameSelector.AddOptions(gameNames);
+    }
+    public void DropdownIndexCghanged(int index)
+    {
+        
+        currentChosenGame = gameNames[index];
+        if (PanellList.Count > 0)
+        {
+            for (int i = 0; i < PanellList.Count; i++)
+            {
+                Destroy(PanellList[i]);
+            }
+        }
+        PanellList.Clear();
+        activeGame.GameName = currentChosenGame;
+        DeserializeActiveGame();
+        CreateStandartGame();
+        CreateGoldGame();
+    }
+    private void DeserializeActiveGame()
+    {
+        FileStream stream = new FileStream(activeGame.GameName + ".xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+        activeGame = serializer.Deserialize(stream) as GamesTable.Game;
+        stream.Close();
+        Debug.Log(activeGame.GameName + " is current chosen");
+        
+    }
+    private void CreateStandartGame()
+    {
+        var standartGame = Instantiate<GameObject>(standartGamePanelPrafab);
+        PanellList.Add(standartGame);
+        Text _coins = standartGame.GetComponentInChildren<Text>();
+        var _progressBar = standartGame.transform.Find("ProgressBar");
+        var _progressText = _progressBar.transform.Find("ProgressText");
+        Text _progress = _progressText.GetComponentInChildren<Text>();
+        Slider currentGold = standartGame.GetComponentInChildren<Slider>();
+        _coins.text = activeGame.CurrentGoldCoints.ToString();
+        currentGold.minValue = 0;
+        currentGold.maxValue = activeGame.StandartGameGoldCosts;
+        currentGold.value = activeGame.CurrentGoldCoints;
+        _progress.text = _coins.text + "/" + currentGold.maxValue.ToString();
+        standartGame.transform.SetParent(canvas.transform,false);
+        CreateStandartRoom();
+    }
+    private void CreateStandartRoom()
+    {
+        for (int i = 0; i <= activeGame.StandatrGameRooms-1; i++)
+        {
+            var standartroom = Instantiate(joinRoomPanelPrefab);
+            PanellList.Add(standartroom);
+            var playerCountBar = standartroom.transform.Find("PlayerCountText");
+            Text playerCountText = playerCountBar.GetComponentInChildren<Text>();
+            var roomNameBar= standartroom.transform.Find("RoomNameText");
+            Text roomNameText = roomNameBar.GetComponentInChildren<Text>();
+            Button joinButton = standartroom.GetComponentInChildren<Button>();
+            playerCountText.text = activeGame.StandartRoomPlayers[i].ToString();
+            roomNameText.text = activeGame.StandartRoomsNames[i].ToString();
+            if(activeGame.StandartRoomPlayers[i]==activeGame.MaxRoomPlayers)
+            {
+                var buttonActive = joinButton.GetComponent<Button>();
+                buttonActive.interactable = false;
+                var joinButtonText = joinButton.GetComponentInChildren<Text>();
+                joinButtonText.text = "Full";
+            }
+            joinButton.onClick.AddListener((UnityEngine.Events.UnityAction)this.JoinToGame);
+            standartroom.transform.SetParent(canvas.transform, false);
+        }
+    }
+    private void CreateGoldGame()
+    {
+        var goldGame = Instantiate<GameObject>(goldGamePanelPrafab);
+        PanellList.Add(goldGame);
+        Text _coins = goldGame.GetComponentInChildren<Text>();
+        var _progressBar = goldGame.transform.Find("ProgressBar");
+        var _progressText = _progressBar.transform.Find("ProgressText");
+        Text _progress = _progressText.GetComponentInChildren<Text>();
+        Slider currentGold = goldGame.GetComponentInChildren<Slider>();
+        _coins.text = activeGame.CurrentGoldCoints.ToString();
+        currentGold.minValue = 0;
+        currentGold.maxValue = activeGame.GoldGameGoldCosts;
+        currentGold.value = activeGame.CurrentGoldCoints;
+        _progress.text = _coins.text + "/" + currentGold.maxValue.ToString();
+        goldGame.transform.SetParent(canvas.transform, false);
+        CreateGoldRoom();
+    }
+    private void CreateGoldRoom()
+    {
+        for (int i = 0; i <= activeGame.GoldGameRooms - 1; i++)
+        {
+            var goldroom = Instantiate(joinRoomPanelPrefab);
+            PanellList.Add(goldroom);
+            var playerCountBar = goldroom.transform.Find("PlayerCountText");
+            Text playerCountText = playerCountBar.GetComponentInChildren<Text>();
+            var roomNameBar = goldroom.transform.Find("RoomNameText");
+            Text roomNameText = roomNameBar.GetComponentInChildren<Text>();
+            Button joinButton = goldroom.GetComponentInChildren<Button>();
+            playerCountText.text = activeGame.GoldRoomPlayers[i].ToString();
+            roomNameText.text = activeGame.GoldRoomsNames[i].ToString();
+            if (activeGame.GoldRoomPlayers[i] == activeGame.MaxRoomPlayers)
+            {
+                var buttonActive = joinButton.GetComponent<Button>();
+                buttonActive.interactable = false;
+                var joinButtonText = joinButton.GetComponentInChildren<Text>();
+                joinButtonText.text = "Full";
+            }
+            joinButton.onClick.AddListener((UnityEngine.Events.UnityAction)this.JoinToGame);
+            goldroom.transform.SetParent(canvas.transform, false);
+        }
+    }
+    public void JoinToGame()
+    {
+        Debug.Log("JOIN TO GAME");
+        SceneManager.LoadScene("JoinRoom", LoadSceneMode.Single);
+    }
+}
